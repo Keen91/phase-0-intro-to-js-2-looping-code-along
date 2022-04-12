27f78ec802bd0230ad95e430993de998bebd063c
keen91 <kinan.hatahet@gmail.com>
2022-04-12 10:55:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+const stringNames = ["Bruce", "James", "John"];
+const eventName = "birthday";
+
+function writeCards(stringNames, eventName) {
+  let cards = [];
+  for (let i = 0; i < stringNames.length; i++) {
+    cards.push(`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return cards;
+}
+
+writeCards(stringNames, eventName);
+
+
+function countDown(integer) {
+    while (integer > 0) {
+        console.log(integer);
+        integer--;
+    }
+    console.log(integer);
+}
+
+countDown(10);
